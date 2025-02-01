603a7ae619cdceb63171ae8301f1567ac4d5d812
Jakov <jakovdelas@gmail.com>
2025-02-02 00:27:55 +0100

    added validation for /login endpoint requests

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, HttpStatus, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -7,7 +8,8 @@ export class AuthController {
 
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    signIn(@Body() signInDto: Record<string, any>) {
+    @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+    signIn(@Body() signInDto: SignInDto) {
       return this.authService.signIn(signInDto.username, signInDto.password);
     }
 }
