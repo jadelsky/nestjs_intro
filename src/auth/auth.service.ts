df7f29555bceb2236f4856ac0848648e1fa435b6
jadelsky <jakovdelas@gmail.com>
2025-02-15 23:42:05 +0100

    test GPG sinature 2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -26,7 +26,6 @@ export class AuthService {
         // TODO: Generate a JWT and return it here
         // instead of the user object
         return result;
-        //Test GPG signing
     }
 
     async create(user: UserCreateDto): Promise<User> {
